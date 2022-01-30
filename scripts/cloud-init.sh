07099c9fb394cd5a12a72edecf55981a0be303d1
Till Wegmueller <toasterson@gmail.com>
2022-01-30 18:50:49 -0300

    make rust installation portable

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -5,22 +5,28 @@ pkg install -v gcc-11 system/header git
 
 git clone https://github.com/Toasterson/metadata-agent.git metadata-agent
 
+# Portable rust installation based on
+# https://www.codejam.info/2015/03/portable-rust-installation.html
+
 wget https://static.rust-lang.org/dist/rust-1.58.1-x86_64-unknown-illumos.tar.gz
 
 tar -xzf rust-1.58.1-x86_64-unknown-illumos.tar.gz
 
-./rust-1.58.1-x86_64-unknown-illumos/install.sh
+mv rust-1.58.1-x86_64-unknown-illumos /opt/rust
 
-# Source Installed Rust environment
-# shellcheck source=./env
-source "$HOME"/.cargo/env
+export LD_LIBRARY_PATH=~/opt/rust/rustc/lib:$LD_LIBRARY_PATH
+export PATH=~/opt/rust/rustc/bin:$PATH
+export PATH=~/opt/rust/cargo/bin:$PATH
 
 cd metadata-agent
 
 MODE=release gmake install
 
-# Setup OS to run console con COM0
+# Setup OS to run console on COM0
 cat <<EOF > /boot/conf.d/cloud-init-serial.conf
 console="ttya"
 autoboot_delay="-1"
 EOF
+
+rm -rf /opt/rust
+rm -rf /root/metadata-agent
